ed516a60ed4d0acbdc0368a9f41e1911a0036166
BBC70n1n1226 <antoniolayon12@hotmail.com>
2025-03-11 11:35:37 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+#define NUM_CARTAS 10
+#define NUM_ATRIBUTOS 4
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+typedef struct {
+    char nome[50];
+    int atributos[NUM_ATRIBUTOS];
+} Carta;
 
-    return 0;
+void inicializarCartas(Carta cartas[]) {
+    char *nomes[NUM_CARTAS] = {"sergipe", "pernanbuco", "sãopaulo", "recife", "ceara", "alagoas", "piaui", "riodejaneiro", "motogrosso", ""};
+    for (int i = 0; i < NUM_CARTAS; i++) {
+        strcpy(cartas[i].nome, nomes[i]);
+        for (int j = 0; j < NUM_ATRIBUTOS; j++) {
+            cartas[i].atributos[j] = rand() % 10000 + 1; // Atributos de 1 a 10000
+        }
+    }
+}
+
+void mostrarCarta(Carta carta) {
+    printf("Nome: %s\n", carta.nome);
+    printf("Atributos:\n");
+    printf("1. Densidade Populacional: %d\n", carta.atributos[0]);
+    printf("2. Área: %d\n", carta.atributos[1]);
+    printf("3. PIB per capita: %d\n", carta.atributos[2]);
+    printf("4. População: %d\n", carta.atributos[3]);
+}
+
+int compararCartas(Carta carta1, Carta carta2, int atributoEscolhido) {
+    if (carta1.atributos[atributoEscolhido] > carta2.atributos[atributoEscolhido]) {
+        return 1;
+    } else if (carta1.atributos[atributoEscolhido] < carta2.atributos[atributoEscolhido]) {
+        return -1;
+    } else {
+        return 0;
+    }
 }
